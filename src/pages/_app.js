af2859cf70c9434164805ce379eb2502e330fd7e
Nazmul Alom <nazmulshuvo03@gmail.com>
2025-06-08 03:12:36 +0600

    landing page removed

--- src/pages/_app.js
+++ src/pages/_app.js
@@ -5,24 +5,8 @@ import { Provider } from "react-redux";
 import { PersistGate } from "redux-persist/integration/react";
 import { config } from "../../.config";
 import { Navigation } from "@/components/Navigation";
-import { useRef } from "react";
 
 export default function App({ Component, pageProps }) {
-  const landingHowItWorksRef = useRef(null);
-  const landingFaqsRef = useRef(null);
-
-  const scrollToHowItWorks = () => {
-    landingHowItWorksRef &&
-      landingHowItWorksRef.current &&
-      landingHowItWorksRef.current.scrollIntoView({ behavior: "smooth" });
-  };
-
-  const scrollToFaqs = () => {
-    landingFaqsRef &&
-      landingFaqsRef.current &&
-      landingFaqsRef.current.scrollIntoView({ behavior: "smooth" });
-  };
-
   return (
     <Provider store={store}>
       <PersistGate loading={null} persistor={persistor}>
@@ -31,7 +15,7 @@ export default function App({ Component, pageProps }) {
             className={`h-screen w-screen overflow-hidden bg-background text-text flex flex-col overflow-y-auto`}
           >
             <div className="w-full">
-              <Navigation {...{ scrollToHowItWorks, scrollToFaqs }} />
+              <Navigation />
             </div>
             <div
               className="flex-1 flex overflow-x-hidden overflow-y-auto"
--- src/pages/index.js
+++ src/pages/index.js
@@ -1,34 +1,9 @@
-import { LandingPage } from "@/components/LandingPage";
-import { useRef } from "react";
-
-// pages/index.js (Landing Page)
-export async function getStaticProps() {
-  // Fetch any data needed for the landing page here
-
-  return {
-    props: {
-      // Pass the data as props to the component
-    },
-  };
-}
-
-const Landing = (props) => {
-  const landingHowItWorksRef = useRef(null);
-  const landingFaqsRef = useRef(null);
-
-  const scrollToHowItWorks = () => {
-    landingHowItWorksRef &&
-      landingHowItWorksRef.current &&
-      landingHowItWorksRef.current.scrollIntoView({ behavior: "smooth" });
-  };
-
-  const scrollToFaqs = () => {
-    landingFaqsRef &&
-      landingFaqsRef.current &&
-      landingFaqsRef.current.scrollIntoView({ behavior: "smooth" });
-  };
-
-  return <LandingPage {...{ landingHowItWorksRef, landingFaqsRef }} />;
+const Landing = () => {
+  return (
+    <div>
+      <h1>Landing Page</h1>
+    </div>
+  );
 };
 
 export default Landing;
